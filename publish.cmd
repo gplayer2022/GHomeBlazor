c63e59045a2542208ece7b3988aab2b6842e3451
gplayer2022 <gplayers@ymail.ne.jp>
2026-05-18 02:09:26 +0900

    [Work] Add publish.cmd.

---
+++ publish.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF62ka$i1yo
+
+dotnet publish -c Release
+
+REM publish結果へ移動
+cd GHomeBlazor/bin/Release/net8.0/publish/wwwroot
+
+REM gh-pagesブランチへデプロイ
+git init
+git remote add github_gplayer2022 git@github_gplayer2022:gplayer2022/GHomeBlazor.git
+git checkout -b gh-pages
+git add .
+git commit -m "deploy by cmd"
+git push -f github_gplayer2022 gh-pages
